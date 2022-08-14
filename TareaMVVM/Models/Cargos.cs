55ca09935ab3298501a831c9708b30ca9cea8a91
ilopez78 <99553835+ilopez78@users.noreply.github.com>
2022-08-14 12:45:58 -0600

    primer repositorio

---
+++ TareaMVVM/Models/Cargos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaMVVM.Models
+{
+    public class Cargos
+    {
+        public string Cargo { get; set; }
+        public static List<Cargos> GetCargos()
+        {
+            var cargo = new List<Cargos>()
+            {
+
+
+                new Cargos(){ Cargo ="CEO" },
+                new Cargos(){ Cargo ="Gerente Financiero" },
+                new Cargos(){ Cargo ="Gerente de Comunicaciones" },
+                 new Cargos(){ Cargo ="Gerente de Marketing" }
+
+            };
+            return cargo;
+        }
+    }
+}
